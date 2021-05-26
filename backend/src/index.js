c7f93f951e933aae435f9770da9f1fedb97fccb6
ChromeCrysis <anderson.bechelli@gmail.com>
2021-05-25 22:27:01 -0300

    backend finalizado e funcional realizando CRUD de Clientes e Ferramentas com Sequelize

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const server = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const clienteRoutes = require('./routes/clienteRoutes')
 const ferramentaRoutes = require('./routes/ferramentaRoutes')
 
+server.use(cors())
 server.use(bodyParser.json())
 server.use('/clientes', clienteRoutes)
 server.use('/ferramentas', ferramentaRoutes)
